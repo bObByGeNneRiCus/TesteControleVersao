4deaaa7ed0031fcf9c7601fa670c6773778f97c3
Rodrigo Camargo <rodrigo.camargo.07@gmail.com>
2023-05-16 21:10:35 -0300

    Teste merge.

--- TesteControleVersao/Program.cs
+++ TesteControleVersao/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dev Leonardo o cara");
+Console.WriteLine("Então tá né...");
