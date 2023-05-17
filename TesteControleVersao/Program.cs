5044ff63187e9db86fe6621129e92696f5659e4e
leonardoschafer <leonardoschafer123@gmail.com>
2023-05-16 21:43:47 -0300

    Agradecimento

--- TesteControleVersao/Program.cs
+++ TesteControleVersao/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dev Leonardo o cara");
 Console.WriteLine("Então tá né...");
 Console.WriteLine("Melhor Academia de Programação Cristã adSm= Rodrigo Senpai");
+Console.WriteLine("Dev/rodrigo obrigado pela aula.");
