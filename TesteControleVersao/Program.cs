19434651697eaded45892d853babead0f53a302f
leonardoschafer <leonardoschafer123@gmail.com>
2023-05-16 21:32:40 -0300

    alteração que da certo

--- TesteControleVersao/Program.cs
+++ TesteControleVersao/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dev Leonardo o cara");
 Console.WriteLine("Então tá né...");
+Console.WriteLine("Melhor Academia de Programação Cristã adSm= Rodrigo Senpai");
