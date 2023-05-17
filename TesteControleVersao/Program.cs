1299c6b4f2cc54bf20037cc8a85979604a9bf229
Rodrigo Camargo <rodrigo.camargo.07@gmail.com>
2023-05-16 21:34:51 -0300

    Ajuste.

--- TesteControleVersao/Program.cs
+++ TesteControleVersao/Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dev Leonardo o cara");
 Console.WriteLine("Então tá né...");
-Console.WriteLine("Melhor Academia de Programação Cristã adSm= Rodrigo Senpai");
+Console.WriteLine("Melhor Academia de Programação Cristã adm= Rodrigo Senpai");
